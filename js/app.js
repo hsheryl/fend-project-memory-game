487792312dc7cddef78692b77fe76fec5f575447
Sheryl Harhsberger <h.sheryl@gmail.com>
2018-05-23 12:27:46 -0700

    feat: Display card on click

--- js/app.js
+++ js/app.js
@@ -75,3 +75,9 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+cardDeck.addEventListener('click', displayCard);
+
+function displayCard(event){
+  event.target.classList.add("open");
+  event.target.classList.add("show");
+}
